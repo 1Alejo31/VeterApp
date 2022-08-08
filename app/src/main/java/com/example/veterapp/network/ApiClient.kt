5de317e706c0b813c76f4e15b1791e807a205160
Krhistian Alejandro Gonzalez Duarte <krhistian.gonzalez@vasslatam.com>
2022-08-07 20:12:14 -0500

    Se actualiza retrofit

--- app/src/main/java/com/example/veterapp/network/ApiClient.kt
+++ app/src/main/java/com/example/veterapp/network/ApiClient.kt
@@ -1,4 +1,10 @@
 package com.example.veterapp.network
 
+import retrofit2.Response
+import retrofit2.http.GET
+
 class ApiClient {
+
+    //Consumir Get para retrofit
+
 }
--- app/src/main/java/com/example/veterapp/ui/view/Beginning.kt
+++ app/src/main/java/com/example/veterapp/ui/view/Beginning.kt
@@ -3,23 +3,11 @@ package com.example.veterapp.ui.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.veterapp.R
-import org.imaginativeworld.whynotimagecarousel.ImageCarousel
-import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 class Beginning : AppCompatActivity() {
-
-    val list = mutableListOf<CarouselItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_beginning)
-
-        val carousel: ImageCarousel = findViewById(R.id.carousel)
-
-        list.add(CarouselItem(R.drawable.reset))
-        list.add(CarouselItem("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcqFuSHurR-dRyTgywDS1HYplrn7JZ3fNzNQ&usqp=CAU"))
-        list.add(CarouselItem(R.drawable.background1))
-
-        carousel.addData(list)
     }
 }
