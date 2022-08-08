555a5b7857e602e30514b51c476d012792d795e2
Krhistian Alejandro Gonzalez Duarte <krhistian.gonzalez@vasslatam.com>
2022-08-07 20:11:06 -0500

    Se agrega retrofit al proyrcto con sus dependencias

---
+++ app/src/main/java/com/example/veterapp/core/RetrofitHelper.kt
@@ -0,0 +1,14 @@
+package com.example.veterapp.core
+
+import retrofit2.converter.gson.GsonConverterFactory
+
+object Retrofit {
+
+    fun getRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://drawsomething-59328-default-rtdb.europe-west1.firebasedatabase.app/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+}
